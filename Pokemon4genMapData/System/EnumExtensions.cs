ee55dea965229e0fc8afa2ede4e14b63d41f82b9
yatsuna827 <yatsuna_827@yahoo.co.jp>
2021-07-27 00:31:41 +0900

    release

---
+++ Pokemon4genMapData/System/EnumExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokemon4genMapData
+{
+    static class EnumExtensions
+    {
+        public static string ToPrefix(this EncounterType encounterType)
+        {
+            if (!Enum.IsDefined(typeof(EncounterType), encounterType))
+                throw new ArgumentException("不正なEncounterTypeです");
+
+            switch (encounterType)
+            {
+                case EncounterType.Grass:
+                    return "grass";
+                case EncounterType.Surf:
+                    return "surf";
+                case EncounterType.OldRod:
+                    return "oldrod";
+                case EncounterType.GoodRod:
+                    return "goodrod";
+                case EncounterType.SuperRod:
+                    return "superrod";
+                case EncounterType.RockSmash:
+                    return "rocksmash";
+                case EncounterType.Headbutt:
+                    return "headbutt";
+                case EncounterType.BugCatching:
+                    return "bugcatching";
+                default:
+                    return "";
+            }
+        }
+
+
+        public static string ToPrefix(this GameVersion version)
+        {
+            if (!Enum.IsDefined(typeof(GameVersion), version))
+                throw new ArgumentException("不正なGameVersionです");
+
+            switch (version)
+            {
+                case GameVersion.Diamond:
+                    return "d";
+                case GameVersion.Pearl:
+                    return "p";
+                case GameVersion.Platinum:
+                    return "pt";
+                case GameVersion.HeartGold:
+                    return "hg";
+                case GameVersion.SoulSilver:
+                    return "ss";
+                default:
+                    return "";
+            }
+        }
+
+
+    }
+}
---
+++ Pokemon4genMapData/System/WrappedEncounterType.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokemon4genMapData
+{
+    interface IWrappedEncounterType<TVersion> : IEnumWapper<EncounterType>
+        where TVersion : IWrappedGameVersion
+    {
+
+    }
+
+    struct WrappedGrass : IWrappedEncounterType<WrappedDPt>, IWrappedEncounterType<WrappedHGSS>
+    {
+        public EncounterType Unwrap() => EncounterType.Grass;
+    }
+    struct WrappedSurf : IWrappedEncounterType<WrappedDPt>, IWrappedEncounterType<WrappedHGSS>
+    {
+        public EncounterType Unwrap() => EncounterType.Surf;
+    }
+    struct WrappedOldRod : IWrappedEncounterType<WrappedDPt>, IWrappedEncounterType<WrappedHGSS>
+    {
+        public EncounterType Unwrap() => EncounterType.OldRod;
+    }
+    struct WrappedGoodRod : IWrappedEncounterType<WrappedDPt>, IWrappedEncounterType<WrappedHGSS>
+    {
+        public EncounterType Unwrap() => EncounterType.GoodRod;
+    }
+    struct WrappedSuperRod : IWrappedEncounterType<WrappedDPt>, IWrappedEncounterType<WrappedHGSS>
+    {
+        public EncounterType Unwrap() => EncounterType.SuperRod;
+    }
+    struct WrappedRockSmash : IWrappedEncounterType<WrappedHGSS>
+    {
+        public EncounterType Unwrap() => EncounterType.RockSmash;
+    }
+    struct WrappedHeadbutt : IWrappedEncounterType<WrappedHGSS>
+    {
+        public EncounterType Unwrap() => EncounterType.Headbutt;
+    }
+    struct WrappedBugCatching : IWrappedEncounterType<WrappedHGSS>
+    {
+        public EncounterType Unwrap() => EncounterType.BugCatching;
+    }
+}
